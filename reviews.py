c16fc56280f7229cc50cf3e79f1c857dd766273f
Your Name <you@example.com>
2024-09-17 19:39:51 -0400

    wine review code/ CSV file

--- reviews.py
+++ reviews.py
@@ -1,2 +1,11 @@
-# add your code here
+import pandas as pd
 
+df = pd.read_csv('data/winemag-data-130k-v2.csv.zip')
+
+country_summary = df.groupby('country').agg(count = ('points', 'size'), points = ('points', 'mean')).reset_index()
+
+country_summary['points'] = country_summary['points'].round(1)
+
+country_summary.to_csv('data/reviews-per-country.csv', index = False)
+
+print(country_summary.head())
